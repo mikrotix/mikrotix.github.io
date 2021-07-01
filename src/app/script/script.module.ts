e6c4284a469eadb1e11c02006296cccfe964dd54
hexatester <revolusi147id@gmail.com>
2021-07-01 12:40:05 +0700

    Delete generate component

--- src/app/script/script.module.ts
+++ src/app/script/script.module.ts
@@ -6,13 +6,11 @@ import { ScriptComponent } from './script.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
-import { GenerateComponent } from './generate/generate.component';
 
 
 @NgModule({
   declarations: [
-    ScriptComponent,
-    GenerateComponent
+    ScriptComponent
   ],
   imports: [
     CommonModule,
